463ebd990186239de3dcba1b2f519ca53659f2c4
Lisa Lehrman <lisa.lehrman@gmail.com>
2017-11-07 16:21:41 -0800

    adds left and right arrow sprites

--- js/main.js
+++ js/main.js
@@ -8,27 +8,39 @@ var GameState = {
 	  this.load.image('background', 'assets/images/background.png');
 	  this.load.image('cat1', 'assets/images/phaser-cat1.png');
 	  this.load.image('rightArrow', 'assets/images/right-arrow.png');
-	  // this.load.image('cat3', 'assets/images/phaser-cat3.png');
+	  this.load.image('leftArrow', 'assets/images/left-arrow.png');
+
 
 
 
 	},
 	create: function(){
-		//define scale
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
+		//defines scale
+	 this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	 this.scale.pageAlignHorizontally = true;
+	 this.scale.pageAlignVertically = true;
 
-		//creates sprites
+	//creates sprites
+	  //bg image
 	  this.background = this.game.add.sprite(0, 0, 'background');
+	  //cat1 sprite
 	  this.cat1 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'cat1');
 	  this.cat1.anchor.setTo(0.5);
-	  this.cat1.scale.setTo(-1,1);
+	  this.cat1.scale.setTo(-.5,.5);
 	  //right arrow
 	  this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'rightArrow');
-	  this.rightArrow.scale.setTo(5);
+	  // this.rightArrow.scale.setTo(2);
+	  this.rightArrow.anchor.setTo(0.5);
+	  this.rightArrow.customParams = {direction: 1};
+	  //left arrow
+	  this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'leftArrow');
+	  this.leftArrow.anchor.setTo(0.5);
+	  this.leftArrow.customParams = {direction: -1};
+	  // this.leftArrow.scale.x.setTo(-1);
+
+
 	},
-	//executed multiple times per second
+	//update function is executed multiple times per second
 	update: function(){
 		//rotates the sprite
 		// this.cat1.angle += 0.5;
